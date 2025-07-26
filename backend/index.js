c91971d0937cd8a5e146253637cf3e63cc87c021
Hymanshu-jha <himanshujhaa212@gmail.com>
2025-07-26 15:00:50 +0530

    added vercel frontend domain to the cors origins

--- backend/index.js
+++ backend/index.js
@@ -26,17 +26,25 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://appointment-booking-system-three.vercel.app'
+    ],
     methods: ['GET', 'POST'],
     credentials: true,
   },
 });
 
+
 // Express CORS middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'https://appointment-booking-system-three.vercel.app'
+  ],
   credentials: true,
 }));
+
 
 app.use(
   session({
