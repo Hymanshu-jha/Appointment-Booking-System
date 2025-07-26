b5a079ef415bc3b05dab06122a30a7a90013c37e
Hymanshu-jha <himanshujhaa212@gmail.com>
2025-07-26 17:07:56 +0530

    removed trailing slash from vercel url from cors

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       'http://localhost:5173',
-      'https://appointment-booking-system-eight.vercel.app/',
+      'https://appointment-booking-system-eight.vercel.app',
     ],
     methods: ['GET', 'POST'],
     credentials: true,
@@ -40,7 +40,7 @@ const io = new Server(server, {
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://appointment-booking-system-eight.vercel.app/',
+    'https://appointment-booking-system-eight.vercel.app',
   ],
   credentials: true,
 }));
