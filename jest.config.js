5796e8dc5b8fa0437e74cefef0eecf80a1b9355d
Hymanshu-jha <himanshujhaa212@gmail.com>
2025-12-18 17:52:41 +0530

    ci(test): add junit and coverage reports for backend tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,19 @@
 export default {
   testEnvironment: "node",
   testMatch: ["**/tests/backend_tests/**/*.test.js"],
-  clearMocks: true
+  clearMocks: true,
+
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        outputDirectory: ".",
+        outputName: "junit.xml"
+      }
+    ]
+  ],
+
+  coverageDirectory: "coverage",
+  collectCoverage: true
 };
